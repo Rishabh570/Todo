f95b3fa9986c5a5aff28fbfca5f06b4df32be030
Rishabh Rawat <www.rishabhrawat570@gmail.com>
2017-07-22 16:54:06 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './js/app.js',
+
+  output: {
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+       }
+     ]
+  }
+};
